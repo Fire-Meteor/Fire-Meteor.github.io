78f73d92dd2e17b0ef673a4eb6677e32421d331d
luffy <zhangfuqiang@zhangfuqiang.com>
2019-08-07 06:51:53 +0800

    add

---
+++ js/time.js
@@ -0,0 +1,28 @@
+﻿function showTime() { 
+	var currentDate = new Date(); 
+	var startDate = new Date(2019, 07, 07);
+	var date3 = currentDate-startDate;
+	var days=Math.floor(date3/(24*3600*1000));
+	var leave1=date3%(24*3600*1000);     
+	var hours=Math.floor(leave1/(3600*1000));
+	var leave2=leave1%(3600*1000);        
+	var minutes=Math.floor(leave2/(60*1000));
+	var leave3=leave2%(60*1000);       
+	var seconds=Math.round(leave3/1000);
+	if (minutes < 10) 
+	minutes = "0" + minutes; 
+	if (seconds < 10) 
+	seconds = "0" + seconds; 
+	var currentTimeString = "<br>"
+		+ "I have been in love with you for:<br> "
+		+ "&nbsp&nbsp&nbsp&nbsp&nbsp"
+		+ "<c style=\"color: #CCFF99; text-shadow:2px 3px 3px #222; font-family:Microsoft YaHei; font-size:24px\" >" + days +" </c>day " 
+		+ "<c style=\"color: #CCFF99; text-shadow:2px 3px 3px #222; font-family:Microsoft YaHei; font-size:24px\" >" + hours+" </c>hour "
+		+"<c style=\"color: #CCFF99; text-shadow:2px 3px 3px #222; font-family:Microsoft YaHei; font-size:24px\" >" + minutes+" </c>min "
+		+"<c style=\"color: #CCFF99; text-shadow:2px 3px 3px #222; font-family:Microsoft YaHei; font-size:24px\" >" +seconds+" </c>sec<br>"
+		+ "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp只愿执子之手,与子偕老.<br>"
+		+ "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp"
+		+	"<c style=\"color: #CCCCCC; text-shadow:2px 3px 3px #222; font-family:Microsoft YaHei; font-size:20px\" >----RIO   2019-08-07</c>"; 
+	document.getElementById("show").innerHTML=currentTimeString;  
+	window.setTimeout("showTime()", 1000); 
+} 
